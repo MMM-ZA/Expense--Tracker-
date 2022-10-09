e9773ad8e8aaa001d6452eae65364027d6c114db
makoma mabilo <makoma44@gmail.com>
2022-10-09 21:14:10 +0800

    passing data via props

--- expense_tracker/src/components/ExpenseItem.js
+++ expense_tracker/src/components/ExpenseItem.js
@@ -1,12 +1,13 @@
 import './ExpenseItem.css';
 
-function ExpenseItem () {
+function ExpenseItem (props) {
+
   return (
  <div className="expense-item">
-  <div>10 October 2022</div>
+  <div>{props.date.toISOString()}</div>
   <div className="expense-item__description">
-     <h2>Rent</h2>
-    <div className="expense-item__price">$700</div>
+     <h2>{props.title}</h2>
+    <div className="expense-item__price">${props.amount}</div>
    </div>
  </div>
  );
