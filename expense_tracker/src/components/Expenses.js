a4b456fca900a55d36be9f6d14a1ed111c3f4aa4
makoma mabilo <makoma44@gmail.com>
2022-10-28 15:48:24 +0800

    added new component ExpensesList js and css files

--- expense_tracker/src/components/Expenses.js
+++ expense_tracker/src/components/Expenses.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
+import ExpensesList from './ExpensesList';
 
-import ExpenseItem from './ExpenseItem';
 import Card from './Card';
 import './Expenses.css';
 import ExpenseFilter from './ExpenseFilter';
@@ -17,26 +17,12 @@ const Expenses = (props) => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
 
-  let expensesContent = <p>No Expenses found</p>;
-
-  if (FilteredExpenses.length > 0) {
-    expensesContent =
-      FilteredExpenses.map((expense) => (
-      <ExpenseItem
-      key={expense.id}
-      title={expense.title}
-      amount={expense.amount}
-      date={expense.date}
-      />
-      ));
-  }
-
   return (
     <div>
 
      <Card className="expenses">
       <ExpenseFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
-      {expensesContent}
+      <ExpensesList items={FilteredExpenses}/>
      </Card>
     </div>
   );
